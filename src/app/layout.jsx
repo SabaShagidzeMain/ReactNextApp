93a83e71c3a3ed6b6223827f88529041ee4dc5e3
SabaShagidze2 <154365489+SabaShagidze2@users.noreply.github.com>
2024-10-11 19:03:46 +0400

    stylistic changes to store page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/prop-types */
 import "./globals.css";
 
-export const metadata = {
-  title: "Gaming App",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Gaming App</title>
+      </head>
       <body>
         <div id="root">{children}</div>
       </body>
