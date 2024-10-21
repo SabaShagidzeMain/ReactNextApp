24a8bed5babbd1b2f1e480e0dbe5a06058359783
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-10-21 18:56:26 +0400

    feat(profile): fetch and display logged-in user's information dynamically

--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -1,102 +1,54 @@
-"use client";
-
 import Header from "@/Components/Header/Header";
 import Footer from "@/Components/Footer/Footer";
 import "./profile.css";
 
-const Profile = () => {
-  function onChange(e) {
-    const inputValue = e.target.value;
-    console.log(inputValue);
-  }
+import { cookies } from "next/headers";
+
+export default async function Profile() {
+  const userCookies = cookies();
+  const accessToken = userCookies.get("accessToken")?.value;
+
+  const res = await fetch("https://dummyjson.com/auth/me", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: "include",
+  });
+
+  const user = await res.json();
+
   return (
     <>
       <Header />
       <main className="main user-main">
         <div className="user-wrapper">
           <div className="user-picture-wrapper">
-            <p className="user-header">James Allan</p>
-            <p className="user-handle">@James</p>
-            <button className="user-button">Upload A New Picture</button>
-            <p className="user-text">
-              Member Since <span>29 September 2019</span>
-            </p>
+            <img className="user-image" src={user.image} alt={user.firstName} />
           </div>
           <div className="user-info-wrapper">
             <p className="user-header">Edit Profile</p>
             <div className="user-info-inner">
               <div className="user-info-col">
                 <div className="user-info-block">
-                  <label htmlFor="fname">First Name:</label>
-                  <input
-                    type="text"
-                    id="fname"
-                    name="fname"
-                    value="John"
-                    onChange={onChange}
-                  />
+                  <label htmlFor="fname">Name</label>
+                  <input type="text" value={user.firstName} readOnly />
                 </div>
                 <div className="user-info-block">
-                  <label htmlFor="pass">Password:</label>
-                  <input
-                    type="password"
-                    id="pass"
-                    name="pass"
-                    value="verysecret"
-                    onChange={onChange}
-                  />
+                  <label htmlFor="pass">Surname</label>
+                  <input type="text" value={user.lastName} readOnly />
                 </div>
                 <div className="user-info-block">
-                  <label htmlFor="email">Email:</label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value="JamesAllan@Mail.Com"
-                    onChange={onChange}
-                  />
+                  <label htmlFor="email">Email</label>
+                  <input type="email" value={user.email} readOnly />
                 </div>
               </div>
-              <div className="user-info-col">
-                <div className="user-info-block">
-                  <label htmlFor="lname">First Name:</label>
-                  <input
-                    type="text"
-                    id="lname"
-                    name="lname"
-                    value="Allan"
-                    onChange={onChange}
-                  />
-                </div>
-                <div className="user-info-block">
-                  <label htmlFor="repeat-pass">Password:</label>
-                  <input
-                    type="password"
-                    id="repeat-pass"
-                    name="prepeat-pass"
-                    value="verysecret"
-                    onChange={onChange}
-                  />
-                </div>
-                <div className="user-info-block">
-                  <label htmlFor="email">Email:</label>
-                  <input
-                    type="confirm-email"
-                    id="confirm-email"
-                    name="confirm-email"
-                    value="JamesAllan@Mail.Com"
-                    onChange={onChange}
-                  />
-                </div>
-              </div>
+              <div className="user-info-col"></div>
             </div>
-            <button className="user-button">Update Info</button>
           </div>
         </div>
       </main>
       <Footer />
     </>
   );
-};
-
-export default Profile;
+}
