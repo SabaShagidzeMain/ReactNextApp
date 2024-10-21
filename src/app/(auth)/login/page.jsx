e073902407151c30d380de1cccb47460b5c92af5
GiorgiGogaladze <gogaladzegio55@gmail.com>
2024-10-21 15:53:55 +0400

    creating POST method functionality and saving token in Js cookies

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,52 @@
-import "./login.css";
+'use client'
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
 
-const loginPage = () => {
-  return <>Login</>;
+
+function Authorization() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      
+      const response = await fetch('https://dummyjson.com/auth/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+    });
+  
+    const data = await response.json(); 
+    
+    if (response.ok && data.accessToken) {
+      Cookies.set('accessToken', data.accessToken, { path: '/', secure: false, sameSite: 'Strict' });
+console.log('token set:', data.accessToken);
+
+    } else {
+        console.log('login failed:', data.message);
+    }
 };
 
-export default loginPage;
+useEffect(() => {
+  const token = Cookies.get('accessToken');
+  if (token) {
+    router.push('/');
+  }
+}, [router]);
+
+  return (
+    <section>
+      
+    </section>
+  );
+}
+
+export default Authorization;
