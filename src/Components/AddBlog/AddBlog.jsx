5e8625b708e79b11721d001d5ba5464b9b5bad39
nikoloz01 <nikaKhaburzania18@gmail.com>
2024-10-24 03:58:09 +0400

    add improved styles to add blogs component

--- src/Components/AddBlog/AddBlog.jsx
+++ src/Components/AddBlog/AddBlog.jsx
@@ -1,10 +1,12 @@
 "use client";
+import { useState } from "react";
 import { apiPost } from "@/Utilities/apiPost";
-import React, { useState } from "react";
+import "./AddBlog.css";
 
 export default function AddBlog({ addNewPost }) {
   const [inputValue, setInputValue] = useState("");
   const [bodyValue, setBodyValue] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,25 +25,33 @@ export default function AddBlog({ addNewPost }) {
     addNewPost(newObject);
     setInputValue("");
     setBodyValue("");
+    setIsVisible(false);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter title"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Enter body"
-          value={bodyValue}
-          onChange={(e) => setBodyValue(e.target.value)}
-        />
-        <button type="submit">Add Post</button>
-      </form>
+      <header className="addPost-header">
+        <button className="show-form-button" onClick={() => setIsVisible(!isVisible)}>
+          {isVisible ? "Close" : "Add Post"}
+        </button>
+      </header>
+      <div className={`form-container ${isVisible ? "visible" : "hidden"}`}>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter title"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Enter body"
+            value={bodyValue}
+            onChange={(e) => setBodyValue(e.target.value)}
+          />
+          <button type="submit">Add Post</button>
+        </form>
+      </div>
     </>
   );
 }
